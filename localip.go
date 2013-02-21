6d66a3b25bdcd6ce380d82e70c797648a24ee5a1
Eryx <evorui@gmail.com>
2013-02-21 16:49:56 +0800

    get local ip address

---
+++ localip.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "net"
+    "fmt"
+)
+
+func main() {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        panic(err)
+    }
+    for _, addr := range addrs {
+        fmt.Println(addr.String())
+    }
+}
