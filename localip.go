5e187a3b37f8dd9ba24c6af7561f76bd0dd00e78
Eryx <evorui@gmail.com>
2013-06-10 16:57:13 +0800

    [NEW] Get local ip from shell

--- localip.go
+++ localip.go
@@ -3,14 +3,27 @@ package main
 import (
     "fmt"
     "net"
+    "bytes"
+    "os/exec"
+    "strings"
 )
 
 func main() {
+    
     addrs, err := net.InterfaceAddrs()
     if err != nil {
         panic(err)
     }
     for _, addr := range addrs {
-        fmt.Println(addr.String())
+        fmt.Println("Method 1:", addr.String())
+    }
+    
+    var out bytes.Buffer
+    cmd := "ip addr|grep inet|grep -v inet6|grep -v 127.0.|head -n1" +
+        "|awk ' {print $2}'|awk -F \"/\" '{print $1}'"
+    ec := exec.Command("sh", "-c", cmd)
+    ec.Stdout = &out
+    if err := ec.Run(); err == nil {
+        fmt.Println("Method 2:", strings.TrimSpace(out.String()))
     }
 }
